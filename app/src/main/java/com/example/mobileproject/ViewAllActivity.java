f47fe2e234303fa47da1e5bcf35d9480501e1493
win10 <song_jie99@hotmail.com>
2022-06-05 22:49:19 +0800

    popular item (fresh veggie and fruit) can use dy

--- app/src/main/java/com/example/mobileproject/ViewAllActivity.java
+++ app/src/main/java/com/example/mobileproject/ViewAllActivity.java
@@ -1,14 +1,79 @@
 package com.example.mobileproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.mobileproject.adapters.ViewAllAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ViewAllActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+
+    RecyclerView recyclerView;
+    ViewAllAdapter viewAllAdapter;
+    List<ViewAllModel> viewAllModelList;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all);
+
+        toolbar = findViewById(R.id.toolbar5);
+        setSupportActionBar(toolbar);
+
+        firestore = FirebaseFirestore.getInstance();
+        String type = getIntent().getStringExtra("type");
+        recyclerView = findViewById(R.id.view_all_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        viewAllModelList = new ArrayList<>();
+        viewAllAdapter = new ViewAllAdapter(this,viewAllModelList);
+        recyclerView.setAdapter(viewAllAdapter);
+
+
+        ///////Getting vegetable
+        if (type != null && type.equalsIgnoreCase("vegetable")){
+            firestore.collection("AllProducts").whereEqualTo("type","vegetable").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                    for(DocumentSnapshot documentSnapshot:task.getResult().getDocuments()){
+                        ViewAllModel viewAllModel = documentSnapshot.toObject(ViewAllModel.class);
+                        viewAllModelList.add(viewAllModel);
+                        viewAllAdapter.notifyDataSetChanged();
+                    }
+                }
+            });
+        }
+
+        ///////Getting fruit
+        if (type != null && type.equalsIgnoreCase("fruit")) {
+            firestore.collection("AllProducts").whereEqualTo("type", "fruit").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                    for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()) {
+                        ViewAllModel viewAllModel = documentSnapshot.toObject(ViewAllModel.class);
+                        viewAllModelList.add(viewAllModel);
+                        viewAllAdapter.notifyDataSetChanged();
+                    }
+                }
+            });
+        }
     }
+
 }
--- app/src/main/java/com/example/mobileproject/adapters/ViewAllAdapter.java
+++ app/src/main/java/com/example/mobileproject/adapters/ViewAllAdapter.java
@@ -39,7 +39,7 @@ public class ViewAllAdapter extends RecyclerView.Adapter<ViewAllAdapter.ViewHold
         holder.name.setText(list.get(position).getName());
         holder.description.setText(list.get(position).getDescription());
         holder.rating.setText(list.get(position).getRating());
-        holder.price.setText(list.get(position).getPrice()+"/kg");
+        holder.price.setText(list.get(position).getPrice() + "/kg");
 
     }
 
@@ -60,6 +60,7 @@ public class ViewAllAdapter extends RecyclerView.Adapter<ViewAllAdapter.ViewHold
             name = itemView.findViewById(R.id.view_name);
             description = itemView.findViewById(R.id.view_description);
             price = itemView.findViewById(R.id.view_price);
+            rating = itemView.findViewById(R.id.view_rating);
         }
     }
 }
