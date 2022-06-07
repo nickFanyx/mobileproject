46e74e1f756bbae8d4652aa5ffaf51a12be621bc
AishahWan <CB19038@student.ump.edu.my>
2022-06-08 04:51:48 +0800

    try to connect with firebase. pls check :)

--- app/src/main/java/com/example/mobileproject/RegistrationActivity.java
+++ app/src/main/java/com/example/mobileproject/RegistrationActivity.java
@@ -1,13 +1,23 @@
 package com.example.mobileproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mobileproject.Model.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -15,10 +25,16 @@ public class RegistrationActivity extends AppCompatActivity {
     EditText name, email, password;
     TextView signIn;
 
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         signUp = findViewById(R.id.signup_btn);
         name = findViewById(R.id.name_reg);
@@ -32,8 +48,65 @@ public class RegistrationActivity extends AppCompatActivity {
                 startActivity(new Intent(RegistrationActivity.this,Login.class));
             }
         });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
 
 
 
     }
+
+    private void createUser() {
+
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if (TextUtils.isEmpty(userName)){
+            Toast.makeText(this, "Name is Empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this, "Password is Empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this, "Password is Empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (userPassword.length() < 6){
+            Toast.makeText(this, "Password length must be greater than 6 letters", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //create User
+        auth.createUserWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+
+                            UserModel userModel = new UserModel(userName, userEmail, userPassword);
+                            String id = task.getResult().getUser().getUid();
+                            database.getReference().child("Users").child(id).setValue(userModel);
+
+                            Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(RegistrationActivity.this, "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+     public void goToLogin(View view){
+        startActivity(new Intent(RegistrationActivity.this, Login.class));
+     }
 }
