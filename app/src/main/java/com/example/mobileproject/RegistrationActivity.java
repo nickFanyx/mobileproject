e5f583ff6d75166729b31280317218d8932f4a8a
AishahWan <CB19038@student.ump.edu.my>
2022-06-08 03:20:16 +0800

    reg adjust

--- app/src/main/java/com/example/mobileproject/RegistrationActivity.java
+++ app/src/main/java/com/example/mobileproject/RegistrationActivity.java
@@ -2,19 +2,37 @@ package com.example.mobileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     Button signUp;
-    TextView name, email, password;
+    EditText name, email, password;
+    TextView signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        signUp = findViewById(R.id.signup_btn);
+        name = findViewById(R.id.name_reg);
+        email = findViewById(R.id.email_reg);
+        password = findViewById(R.id.password_reg);
+        signIn = findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this,Login.class));
+            }
+        });
+
 
 
     }
