606a294b534cf113241d7b070d116294996466d2
AishahWan <CB19038@student.ump.edu.my>
2022-06-11 18:25:18 +0800

    fingerprint login

--- app/src/main/java/com/example/mobileproject/Login.java
+++ app/src/main/java/com/example/mobileproject/Login.java
@@ -1,7 +1,13 @@
 package com.example.mobileproject;
 
+import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +23,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.concurrent.Executor;
+
 public class Login extends AppCompatActivity {
 
     Button signIn;
@@ -24,6 +32,8 @@ public class Login extends AppCompatActivity {
     TextView signUp;
 
     FirebaseAuth auth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +46,8 @@ public class Login extends AppCompatActivity {
         email = findViewById(R.id.email_login);
         password = findViewById(R.id.password_login);
         signUp = findViewById(R.id.sign_up);
+        TextView msg_text = findViewById(R.id.txt_msg);
+        Button Fingerprint_btn = findViewById(R.id.Fingerprint_btn);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +62,63 @@ public class Login extends AppCompatActivity {
 
                 loginUser();
 
+            }
+        });
+        //Create the Biometric
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                msg_text.setText("You can use the fingerprint sensor to login");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                msg_text.setText("The device don't have a fingerprint sensor");
+                Fingerprint_btn.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                msg_text.setText("The biometric sensor is currently unavailable");
+                Fingerprint_btn.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                msg_text.setText("Your device don't have any fingerprint saved, please check your security setting");
+                Fingerprint_btn.setVisibility(View.GONE);
+                break;
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+        //create the biometric prompt callback
+        BiometricPrompt biometricPrompt = new BiometricPrompt(Login.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override //this method is called when there is an error while the authentication
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override //this method is called when the authentication is a success
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+
+                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Login.this, HomeCategory.class));
+
+            }
+
+            @Override //this method is called if we have failed the authentication
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        //Create biometric Dialog
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Login")
+                .setDescription("Use your fingerprint to login to your app")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        //call the dialog when the user press the login button
+        Fingerprint_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                biometricPrompt.authenticate(promptInfo);
             }
         });
 
@@ -81,8 +150,9 @@ public class Login extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if (task.isSuccessful()){
-                            startActivity(new Intent(Login.this, MainActivity.class));
-                            Toast.makeText(Login.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Login.this, HomeCategory.class));
+
+
                         }
                         else{
                             Toast.makeText(Login.this, "Error"+task.isSuccessful(), Toast.LENGTH_SHORT).show();
